d293812c6403140d1bdc1cbc40dfdfb9275ce6bd
david1768 <davidsplaystation1011@gmail.com>
2026-01-27 09:20:09 -0600

    Update 3_return.py

--- 3_return.py
+++ 3_return.py
@@ -1,10 +1,9 @@
 # Return Practice #1
 # Create a function called power that takes two numeric values as arguments. It must return the number that results from solving a power, using the first number as the base, and the second as the exponent
 
-
-
-
-
+def power(num1,num2):
+    return(x ** y)
+    return()
 
 
 # Return Practice #2
@@ -14,7 +13,11 @@
 
 # Hint: to perform the conversion, the function internally must multiply this value in dollars by 0.90 to obtain the equivalent amount in euros.
 
-
+def usd_to_eur(usd):
+    euro-(usd*.84
+    print (euro)
+    return usd_to_eur
+usd_to_eur(4)
 
 
 # Return Practice #3
@@ -24,4 +27,9 @@
 
 # Also, you must create a variable called word, which contains any string, to pass it as an argument to the created function.
 
-# Hint: inside the created function, you should use string methods already seen.
+# Hint: inside the created function, you should use string methods already seen.\
+
+def reverseword(word):
+    reverse-word[::-1]
+    return(reverse)
+reverseword("hello")
