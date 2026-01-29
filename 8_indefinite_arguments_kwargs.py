749950501084845eed3329734145cc05537e606e
david1768 <davidsplaystation1011@gmail.com>
2026-01-29 09:16:49 -0600

    Update 8_indefinite_arguments_kwargs.py

--- 8_indefinite_arguments_kwargs.py
+++ 8_indefinite_arguments_kwargs.py
@@ -2,7 +2,8 @@
 # Indefinite Arguments (**kwargs) Practice #1
 # Create a function called number_attributes that counts the number of parameters that are passed, and returns that number as the result.
 
-
+def number_attributes(**kwargs):
+    return len(kwargs)
 
 
 
@@ -14,6 +15,8 @@
 # Create a function called list_attributes that returns in the form of a list the values of the attributes given in the form of keywords. The function must expect to receive any number of arguments of this type.
 
 
+def list_attributes(**kwargs):
+    return list(kwargs.values())
 
 
 
@@ -38,3 +41,8 @@
 # Characteristics of Ash:
 # eye_color: brown
 # hair_color: black
+
+def describe_person(name, **kwargs):
+    print(f"Characteristics of {name}:")
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
