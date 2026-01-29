c82f04642bb09959dc5e2af11802b587e040f830
david1768 <davidsplaystation1011@gmail.com>
2026-01-29 08:58:20 -0600

    Update 7_indefinite_arguments_args.py

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,11 +1,44 @@
+def tea_order(cutstomer_name, tea_type, *args, **kwargs):
+    print(cutstomer_name, "ordered a", tea_type, "tea")
+    for arg in args:
+        print(  -Add", arg)
+
+for key, value in kwargs.items():
+    print("  -Add", key, ":", value)
+
+
+tea_order("Alice","chamomile")
+tea_order("Bob""Black",milk= "oak")
+tea_order("Tony", "black,"milk="oak"sweetener="honey")
+
+eves_extras =  {"milk": "almond", "sweetener": "sugar", "flavor": "lemon"}
+
+tea_order("Eve", "green", milk= "almond", sweetener="sugar", flavor="lemon")
+tea_order("Eve", "green", **Evese_extras)
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
-
+def sum_squares(*args):
+    total = 0 #initialize total to 0
+    for num in args:  #iterate through each arguement
+        total += num **2 #add the square of the number to total
+        
 # For example for the arguments sum_squares(1,2,3) it should return 14 (1+4+9).
 
-
+return total  #return the total sum of squares
+print(sum_squares(1,2,3)) #exampel usage
+print(sum_squares(4, 5, 6, 7, 7, 8 ,9)) #example usage
 # Indefinite Arguments (*args) Practice #2
 # Create a function called absolute_sum, which takes any number of arguments, and returns the sum of their absolute values (that is, it takes the non-negative values and adds them together, in other words, considers them all - negative and positive - as positive).
+
+def absolute_sum(*args):
+    total= 0 
+    for num in args:
+        total += abs(num)
+
+    return total
+    print(absolute_sum(-1,2,-3))
+    print(absolute_sum(-10, 20, -30,  40 , -50))
 
 # Indefinite Arguments (*args) Practice #3
 # Create a function called personal_numbers that receives, as its first argument, a name, and then an indefinite number of values.
@@ -13,3 +46,7 @@
 # The function should return the following message:
 
 # "{name}, the sum of your numbers is {sum_numbers}"
+
+def personal_numbers(name, *args):
+    total = sum(args)
+    return f"{name}, the sum of your numbers is {total}"
